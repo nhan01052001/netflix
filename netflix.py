d4caf877ee79d5e864f53b0662ba3ba5300c83f6
nhan.nguyen <nhan.nguyen@vnresource.local>
2024-12-26 16:31:07 +0700

    first commit

---
+++ netflix.py
@@ -0,0 +1,13 @@
+import webbrowser
+
+# URL bạn muốn mở
+url = "https://www.facebook.com/"
+
+# Mở trình duyệt
+webbrowser.open(url)
+
+# Đợi một chút để người dùng thấy log
+time.sleep(3)
+
+# In thông tin log khác
+print("MÁY TÍNH CỦA BẠN ĐÃ BỊ HACK!")
